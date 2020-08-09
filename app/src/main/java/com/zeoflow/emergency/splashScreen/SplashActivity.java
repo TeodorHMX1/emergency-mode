e68c8c2f09a203e0802407e230113e6190a5a156
Teodor G <grigor.teodor@gmail.com>
2020-08-09 11:22:29 +0300

    splash screen created

---
+++ app/src/main/java/com/zeoflow/emergency/splashScreen/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.zeoflow.emergency.splashScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zeoflow.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
