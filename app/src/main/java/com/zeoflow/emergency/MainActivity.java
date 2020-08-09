cb64f06372b515fe837d1f4017a7068320a58670
Teodor G <grigor.teodor@gmail.com>
2020-08-09 13:25:59 +0300

    added 3 alarm modes

--- app/src/main/java/com/zeoflow/emergency/MainActivity.java
+++ app/src/main/java/com/zeoflow/emergency/MainActivity.java
@@ -2,7 +2,9 @@ package com.zeoflow.emergency;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -30,7 +32,11 @@ public class MainActivity extends AppCompatActivity
                 .setMaxStreams(1)
                 .build();
 
-        sound = soundPool.load(this, R.raw.emergency, 1);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.alarm_1;
+        MediaPlayer mp = MediaPlayer.create(this, Uri.parse(path));
+        long duration = mp.getDuration() / 100 + 100;
+
+        sound = soundPool.load(this, R.raw.alarm_1, 1);
 
         soundPool.setOnLoadCompleteListener((soundPool, soundID, i1) ->
         {
@@ -46,11 +52,11 @@ public class MainActivity extends AppCompatActivity
                     soundPool.play(sound, volume, volume, 0, 1, 1F);
                     if (volume < 1f)
                     {
-                        volume += 0.001f;
+                        volume += 0.005f;
                     }
-                    mHandler.postDelayed(runnable, 1500);
+                    mHandler.postDelayed(runnable, duration);
                 };
-                mHandler.postDelayed(runnable, 1500);
+                mHandler.postDelayed(runnable, duration);
             }
         });
 
