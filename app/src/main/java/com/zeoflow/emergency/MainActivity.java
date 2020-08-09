ac1e4c1a2741f55889967bc512b81358bff78aa3
Teodor G <grigor.teodor@gmail.com>
2020-08-09 13:10:29 +0300

    updated alarm

--- app/src/main/java/com/zeoflow/emergency/MainActivity.java
+++ app/src/main/java/com/zeoflow/emergency/MainActivity.java
@@ -4,17 +4,21 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.zeoflow.emergency.utils.Utils.STREAM_MUSIC;
-import static com.zeoflow.emergency.utils.Utils.STREAM_NOTIFICATION;
 
 public class MainActivity extends AppCompatActivity
 {
 
     int sound;
     private SoundPool soundPool;
+    float volume = 0.01f;
+    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,15 +30,27 @@ public class MainActivity extends AppCompatActivity
                 .setMaxStreams(1)
                 .build();
 
-        sound = soundPool.load(this, R.raw.alarm, 1);
+        sound = soundPool.load(this, R.raw.emergency, 1);
 
         soundPool.setOnLoadCompleteListener((soundPool, soundID, i1) ->
         {
             if (sound == soundID)
             {
-                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-                audioManager.setStreamVolume(STREAM_MUSIC, audioManager.getStreamMaxVolume(STREAM_MUSIC), 0);
-                soundPool.play(sound, 1f, 1f, 0, 999, 1F);
+                Handler mHandler = new Handler(Looper.getMainLooper());
+
+                runnable = () ->
+                {
+
+                    AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                    audioManager.setStreamVolume(STREAM_MUSIC, audioManager.getStreamMaxVolume(STREAM_MUSIC), 0);
+                    soundPool.play(sound, volume, volume, 0, 1, 1F);
+                    if (volume < 1f)
+                    {
+                        volume += 0.001f;
+                    }
+                    mHandler.postDelayed(runnable, 1500);
+                };
+                mHandler.postDelayed(runnable, 1500);
             }
         });
 
