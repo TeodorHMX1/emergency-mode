0f939773e49c8fc5779cb9d988276c3bc3146b9e
Teodor G <grigor.teodor@gmail.com>
2020-08-09 11:45:37 +0300

    integeres added

--- app/src/main/java/com/zeoflow/emergency/MainActivity.java
+++ app/src/main/java/com/zeoflow/emergency/MainActivity.java
@@ -6,36 +6,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static com.zeoflow.emergency.utils.Utils.STREAM_SYSTEM;
+
 public class MainActivity extends AppCompatActivity
 {
-    /**
-     * The audio stream for phone calls
-     */
-    public static final int STREAM_VOICE_CALL = AudioManager.STREAM_VOICE_CALL;
-    /**
-     * The audio stream for system sounds
-     */
-    public static final int STREAM_SYSTEM = AudioManager.STREAM_SYSTEM;
-    /**
-     * The audio stream for the phone ring
-     */
-    public static final int STREAM_RING = AudioManager.STREAM_RING;
-    /**
-     * The audio stream for music playback
-     */
-    public static final int STREAM_MUSIC = AudioManager.STREAM_MUSIC;
-    /**
-     * The audio stream for alarms
-     */
-    public static final int STREAM_ALARM = AudioManager.STREAM_ALARM;
-    /**
-     * The audio stream for notifications
-     */
-    public static final int STREAM_NOTIFICATION = AudioManager.STREAM_NOTIFICATION;
-    /**
-     * The audio stream for DTMF Tones
-     */
-    public static final int STREAM_DTMF = AudioManager.STREAM_DTMF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
--- app/src/main/java/com/zeoflow/emergency/utils/Utils.java
+++ app/src/main/java/com/zeoflow/emergency/utils/Utils.java
@@ -1,5 +1,36 @@
 package com.zeoflow.emergency.utils;
 
+import android.media.AudioManager;
+
 public class Utils
 {
+    /**
+     * The audio stream for phone calls
+     */
+    public static final int STREAM_VOICE_CALL = AudioManager.STREAM_VOICE_CALL;
+    /**
+     * The audio stream for system sounds
+     */
+    public static final int STREAM_SYSTEM = AudioManager.STREAM_SYSTEM;
+    /**
+     * The audio stream for the phone ring
+     */
+    public static final int STREAM_RING = AudioManager.STREAM_RING;
+    /**
+     * The audio stream for music playback
+     */
+    public static final int STREAM_MUSIC = AudioManager.STREAM_MUSIC;
+    /**
+     * The audio stream for alarms
+     */
+    public static final int STREAM_ALARM = AudioManager.STREAM_ALARM;
+    /**
+     * The audio stream for notifications
+     */
+    public static final int STREAM_NOTIFICATION = AudioManager.STREAM_NOTIFICATION;
+    /**
+     * The audio stream for DTMF Tones
+     */
+    public static final int STREAM_DTMF = AudioManager.STREAM_DTMF;
+
 }
