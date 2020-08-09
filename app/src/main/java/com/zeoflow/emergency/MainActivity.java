a81165f22cc7eff19c6683fb0db27a6a822b2eef
Teodor G <grigor.teodor@gmail.com>
2020-08-09 12:29:49 +0300

    play-sound

--- app/src/main/java/com/zeoflow/emergency/MainActivity.java
+++ app/src/main/java/com/zeoflow/emergency/MainActivity.java
@@ -2,14 +2,19 @@ package com.zeoflow.emergency;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static com.zeoflow.emergency.utils.Utils.STREAM_SYSTEM;
+import static com.zeoflow.emergency.utils.Utils.STREAM_MUSIC;
+import static com.zeoflow.emergency.utils.Utils.STREAM_NOTIFICATION;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    int sound;
+    private SoundPool soundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,8 +22,28 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        audioManager.setStreamVolume(STREAM_SYSTEM, audioManager.getStreamMaxVolume(STREAM_SYSTEM), 0);
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(1)
+                .build();
 
+        sound = soundPool.load(this, R.raw.alarm, 1);
+
+        soundPool.setOnLoadCompleteListener((soundPool, soundID, i1) ->
+        {
+            if (sound == soundID)
+            {
+                AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setStreamVolume(STREAM_MUSIC, audioManager.getStreamMaxVolume(STREAM_MUSIC), 0);
+                soundPool.play(sound, 1f, 1f, 0, 999, 1F);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        soundPool.release();
     }
 }
